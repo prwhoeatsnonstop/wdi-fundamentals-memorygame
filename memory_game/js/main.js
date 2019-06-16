51c208157ba865e91da5bd58b91a965777bfdde3
eunicelok <eunicelp511@gmail.com>
2019-06-16 16:49:09 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -10,12 +10,15 @@ var cardOne = cards[0];
 
 cardsInPlay.push(cardOne);
 
-var cardTwo = cards[3];
+var cardTwo = cards[2];
 
 cardsInPlay.push(cardTwo);
 
-console.log("User flipped " + " " + cardOne);
-console.log ("User flipped " + " " + cardTwo);
+
+
+console.log("User flipped " + cardOne);
+
+console.log("User flipped " + cardTwo);
 
 if (cardsInPlay.length === 2) {
     if (cardsInPlay [0] === cardsInPlay[1]) {
